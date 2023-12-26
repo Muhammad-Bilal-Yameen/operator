e88f6f837bb8678a1e65628f22fe5a633bd6645f
muhammad bilal yameen <bilalyameen03151190023@gmail.com>
2023-12-26 01:06:17 -0800

    half work done

---
+++ main.js
@@ -0,0 +1,22 @@
+// operator
+// Arithmetich operator
+// Adition mean izafa
+var n1 = 1;
+var n2 = 2;
+console.log(n1 + n2);
+// Examle 2
+var b1 = "1";
+var b2 = "2";
+console.log(b1 + b2);
+//subtraction
+var m = 5;
+var n = 4;
+console.log(m - n);
+//Multiplication
+var g = 12;
+var h = 12;
+console.log(g * h);
+//Divition
+var l = 100;
+var j = 10;
+console.log(l / j);
---
+++ main.ts
@@ -0,0 +1,37 @@
+// operator
+                    
+// Arithmetich operator
+
+// Adition mean izafa
+
+
+let n1 = 1
+let n2 = 2
+console.log(n1+n2)
+
+// Examle 2
+
+let b1 = "1"
+let b2 = "2"
+console.log(b1+b2)
+
+
+//subtraction
+
+
+let m = 5
+let n = 4
+console.log(m-n)
+ 
+
+//Multiplication
+
+let g = 12
+let h = 12
+console.log(g*h)
+
+//Divition
+
+let l = 100
+let j = 10
+console.log (l/j)
